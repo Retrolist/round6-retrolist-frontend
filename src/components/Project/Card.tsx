dcfb3d71f92475d7ed8e4ee47ea7672a466fc5b1
china555 <kittikornchina2014@gmail.com>
2023-10-23 13:12:21 +0700

    feat: Update like in card

--- src/components/Project/Card.tsx
+++ src/components/Project/Card.tsx
@@ -1,17 +1,26 @@
-import { LOREM } from "../../utils/lorem";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 export const ProjectCard = () => {
   return (
-    <div className="rounded-2xl bg-white border border-[#EAECF0] p-4 w-full transition hover:cursor-pointer hover:border-gray-400">
-      <div className="flex gap-2 items-center mb-2">
-        <img
-          className="rounded-full w-[34px] h-[34px]"
-          src="/img/project-test-logo.png"
-        />
-        <div className="text-lg">chomtana.eth</div>
+    <div className="relative rounded-2xl bg-white border border-[#EAECF0] p-4 w-full transition hover:cursor-pointer hover:border-gray-400">
+      <div className="flex items-center justify-between">
+        <div className="flex gap-2 items-center mb-2">
+          <img
+            className="rounded-full w-[34px] h-[34px]"
+            src="/img/project-test-logo.png"
+          />
+          <div className="text-lg">ODTU Blockchain</div>
+        </div>
+        <div className="flex items-center gap-2 text-[#4C4E64AD]">
+          <div>12</div>
+          <Icon icon="lucide:heart" />
+        </div>
       </div>
 
-      <div className="text-[#4C4E64AD] line-clamp-2 text-sm mb-3">{LOREM}</div>
+      <div className="text-[#4C4E64AD] line-clamp-2 text-sm mb-3">
+        1W3 is a linktree alternative for the decentralized web. Your personal,
+        censorship-resistant hub to showcase
+      </div>
       <div className="my-3 flex gap-1 items-center">
         <img className="rounded-full w-4 h-4" src="/img/test-avatar.png" />
         <div className="text-[10px] text-[#4C4E64AD]">0x2FAe...e474 </div>
