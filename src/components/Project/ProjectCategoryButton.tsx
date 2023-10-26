5e3f842d1e18b06b707cc5df130787dd3e9d6ae8
Chomtana <Chomtana001@gmail.com>
2023-10-26 11:42:10 +0700

    Implement category filter

---
+++ src/components/Project/ProjectCategoryButton.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react"
+
+interface ProjectCategoryButtonProps {
+  text: string
+  amount?: number
+  categories: string[]
+  category: string
+  setCategory: (category: string) => any
+}
+
+export const ProjectCategoryButton = ({ text, amount, categories, category, setCategory }: ProjectCategoryButtonProps) => {
+  const [ active, setActive ] = useState(false)
+
+  useEffect(() => {
+    if (!category && categories.length == 0) {
+      setActive(true)
+    } else if (category && categories.indexOf(category) != -1) {
+      setActive(true)
+    } else {
+      setActive(false)
+    }
+  }, [ categories ])
+
+  return (
+    <button
+      className={"p-2 flex px-4 rounded-xl transition " + (active ? "bg-[#323A43] text-white" : "bg-[#F5F5F5] text-[#202327] hover:bg-gray-300")}
+      onClick={() => setCategory(category)}
+    >
+      <div>{text}</div>
+      {amount && <div className="bg-white rounded-xl ml-2 px-2 py-1 text-xs text-[#202327]">{amount}</div>}
+    </button>
+  )
+}
--- src/pages/lists/ListsPage.tsx
+++ src/pages/lists/ListsPage.tsx
@@ -1,13 +1,22 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Layout from "../../components/Layout";
 import LayoutSideInfo from "../../components/LayoutSideInfo";
 import { ProjectCard } from "../../components/Project/Card";
 import { useProjects } from "../../hooks/useProjects";
+import { ProjectCategoryButton } from "../../components/Project/ProjectCategoryButton";
 
 export default function ListsPage() {
   const [ search, setSearch ] = useState('')
-  const [ categories, setCategories ] = useState([])
+  const [ categories, setCategories ] = useState<string[]>([])
   const [ seed, setSeed ] = useState(Math.floor(Math.random() * 1000000000).toString())
+
+  const setCategory = useCallback((category?: string) => {
+    if (category) {
+      setCategories([category])
+    } else {
+      setCategories([])
+    }
+  }, [setCategories])
 
   const {
     projects,
@@ -37,26 +46,46 @@ export default function ListsPage() {
             </div>
           </div>
           <div className="flex gap-2 items-center mb-8">
-            <button className="p-2 px-4 rounded-xl bg-[#323A43] text-white">
-              All Project
-            </button>
+            <ProjectCategoryButton
+              text="All Projects"
+              categories={categories}
+              category=""
+              setCategory={setCategory}
+            />
+            
             <div className="border-l-[1px] border border-[#CBD5E0] h-4"></div>
-            <button className="p-2 flex gap-1 px-4 rounded-xl bg-[#F5F5F5] text-[#202327]">
-              <div>OP Stack</div>
-              <div className="bg-white rounded-xl px-2 py-1 text-xs">168</div>
-            </button>
-            <button className="p-2 flex gap-1 px-4 rounded-xl bg-[#F5F5F5] text-[#202327]">
-              <div>Collective Governance</div>
-              <div className="bg-white rounded-xl px-2 py-1 text-xs">168</div>
-            </button>
-            <button className="p-2 flex gap-1 px-4 rounded-xl bg-[#F5F5F5] text-[#202327]">
-              <div>Developer Ecosystem</div>
-              <div className="bg-white rounded-xl px-2 py-1 text-xs">12</div>
-            </button>
-            <button className="p-2 flex gap-1 px-4 rounded-xl bg-[#F5F5F5] text-[#202327]">
-              <div>End user UX</div>
-              <div className="bg-white rounded-xl px-2 py-1 text-xs">12</div>
-            </button>
+
+            <ProjectCategoryButton
+              text="Collective Governance"
+              amount={609}
+              categories={categories}
+              category="COLLECTIVE_GOVERNANCE"
+              setCategory={setCategory}
+            />
+
+            <ProjectCategoryButton
+              text="OP Stack"
+              amount={603}
+              categories={categories}
+              category="OP_STACK"
+              setCategory={setCategory}
+            />
+
+            <ProjectCategoryButton
+              text="Developer Ecosystem"
+              amount={665}
+              categories={categories}
+              category="DEVELOPER_ECOSYSTEM"
+              setCategory={setCategory}
+            />
+
+            <ProjectCategoryButton
+              text="End user UX"
+              amount={1240}
+              categories={categories}
+              category="END_USER_EXPERIENCE_AND_ADOPTION"
+              setCategory={setCategory}
+            />
           </div>
 
           <div className="grid grid-cols-3 gap-4">
