8e2b523ceaa33b2d6773dc195a2dce499ab74b08
Chomtana <Chomtana001@gmail.com>
2023-10-30 17:54:16 +0700

    Fix bug useProjects react hook

--- src/pages/lists/ListsPage.tsx
+++ src/pages/lists/ListsPage.tsx
@@ -113,7 +113,7 @@ export default function ListsPage() {
             hasMore={hasNext}
             loader={<div>Loading...</div>}
           >
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4" style={{ minHeight: '100vh' }}>
               {projects.map((project) => (
                 <ProjectCard project={project} />
               ))}
