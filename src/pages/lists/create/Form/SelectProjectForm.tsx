6ef88517fa15f8a1a53b9291a5ebf6ab0a46c562
Chomtana <Chomtana001@gmail.com>
2023-11-07 23:25:14 +0700

    Integrate project search

--- src/pages/lists/create/Form/SelectProjectForm.tsx
+++ src/pages/lists/create/Form/SelectProjectForm.tsx
@@ -7,38 +7,66 @@ import PrimaryButton from "../../../../components/buttons/PrimaryButton";
 import SecondaryButton from "../../../../components/buttons/SecondaryButton";
 import { useCreateListReducer } from "../../../../stores/CreateListReducer";
 import { SelectProjectTable } from "./SelectProjectTable";
+import { useEffect, useMemo, useState } from "react";
+import { useProjects } from "../../../../hooks/useProjects";
 
 export const SelectProjectForm = () => {
   const navigate = useNavigate();
   const [form] = Form.useForm();
-  const options = [
-    {
-      value: "test1",
-      label: "test1",
-      img: "/img/platform/1.png",
-      description:
-        " RetroPGF Rubric-based List Creation UI opening to public crowdsourcing",
-    },
-    {
-      value: "test2",
-      label: "test2",
-      img: "/img/platform/1.png",
-      description:
-        " RetroPGF Rubric-based List Creation UI opening to public crowdsourcing",
-    },
-    {
-      value: "test3",
-      label: "test3",
-      img: "/img/platform/1.png",
-      description:
-        " RetroPGF Rubric-based List Creation UI opening to public crowdsourcing",
-    },
-  ];
+  const [search, setSearch] = useState("")
+
+  const categories = useMemo(() => [], [])
+
+  const { projects } = useProjects({
+    search,
+    categories: categories,
+  })
+
+  const [options, setOptions] = useState<any[]>([])
+
+  useEffect(() => {
+    if (projects.length > 0) {
+      setOptions(
+        projects.map(project => ({
+          value: project.id,
+          label: project.displayName,
+          img: project.profileImageUrl,
+          description: project.bio,
+        }))
+      )
+    }
+  }, [projects])
+
+  // const options = [
+  //   {
+  //     value: "test1",
+  //     label: "test1",
+  //     img: "/img/platform/1.png",
+  //     description:
+  //       " RetroPGF Rubric-based List Creation UI opening to public crowdsourcing",
+  //   },
+  //   {
+  //     value: "test2",
+  //     label: "test2",
+  //     img: "/img/platform/1.png",
+  //     description:
+  //       " RetroPGF Rubric-based List Creation UI opening to public crowdsourcing",
+  //   },
+  //   {
+  //     value: "test3",
+  //     label: "test3",
+  //     img: "/img/platform/1.png",
+  //     description:
+  //       " RetroPGF Rubric-based List Creation UI opening to public crowdsourcing",
+  //   },
+  // ];
+
   const [state, dispatch] = useCreateListReducer();
 
   const handleChange = (value: string[]) => {
     console.log(`selected ${value}`);
   };
+
   return (
     <div className="p-6 bg-white rounded-lg border border-[#EAECF0]">
       <div className="flex gap-3">
@@ -62,17 +90,20 @@ export const SelectProjectForm = () => {
         <Select
           className="mt-6"
           size="large"
-          mode="multiple"
+          // mode="multiple"
+          showSearch
+          filterOption={false}
           allowClear
           style={{ width: "100%" }}
           placeholder="Search project"
           defaultValue={[]}
           onChange={handleChange}
           tagRender={(props) => <></>}
+          onSearch={(search) => setSearch(search)}
         >
           {options.map((option) => {
             return (
-              <Option value={option.value} className="my-1">
+              <Option value={option.value} key={option.value} className="my-1">
                 <div className="flex items-center gap-2">
                   <img
                     className="h-8 w-8 rounded-full ring-2 ring-white"
