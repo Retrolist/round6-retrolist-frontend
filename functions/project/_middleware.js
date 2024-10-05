e1537d75c86e41a9a540bbc2815c302118159131
Chomtana <Chomtana001@gmail.com>
2024-10-05 17:10:20 +0700

    Round 6 dynamic OG test

--- functions/project/_middleware.js
+++ functions/project/_middleware.js
@@ -14,8 +14,10 @@ class RemoveHandler {
 }
 
 export async function onRequest(context) {
-  const { request, next } = context;
+  const { request, next, env } = context;
   const { pathname } = new URL(request.url);
+
+  const round = env.VITE_CURRENT_ROUND
 
   const pathParts = pathname.split("/");
   const projectId = pathParts[pathParts.length - 1];
@@ -26,12 +28,28 @@ export async function onRequest(context) {
   // Load project data
   try {
     const response = await fetch(
-      `https://round5-api-eas.retrolist.app/projects/${projectId}`
+      `https://round${round}-api-eas.retrolist.app/projects/${projectId}`
     );
     project = await response.json();
   } catch (err) {}
 
   if (!project) return res;
+
+  // Check if og image exists
+  let ogExists = false;
+  const defaultOg = `https://round${round}.retrolist.app/img/cover-r${round}-discovery.png`
+  const dynamicOg = `https://round${round}.retrolist.app/og/${projectId}.png`
+  try {
+    const response = await fetch(
+      `https://round${round}.retrolist.app/og/${projectId}.png`
+    );
+    
+    if (response.ok) {
+      const contentType = response.headers.get('content-type');
+      ogExists = contentType && contentType.includes('image/png')
+    }
+  } catch (err) {}
+  const ogImg = ogExists ? dynamicOg : defaultOg
 
   let ogtag;
 
@@ -42,26 +60,26 @@ export async function onRequest(context) {
     <title>${project.displayName} - RetroList</title>
 
     <meta property="og:title" content="${project.displayName} - RetroList" />
-    <meta property="og:description" content="RetroList | Retro Funding 5 Project Discovery and Community Voting UI" />
+    <meta property="og:description" content="RetroList | Retro Funding ${round} Project Discovery and Community Voting UI" />
     <meta property="og:locale" content="en_US" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="${request.url}" />
-    <meta property="og:image" content="https://retrolist.app/og/${projectId}.png" />
+    <meta property="og:image" content="${ogImg}" />
 
     <meta property="og:image:height" content="630" />
     <meta property="og:image:width" content="1200" />
 
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content="${project.displayName} - RetroList" />
-    <meta name="twitter:description" content="RetroList | Retro Funding 5 Project Discovery and Community Voting UI" />
+    <meta name="twitter:description" content="RetroList | Retro Funding ${round} Project Discovery and Community Voting UI" />
 
-    <meta name="description" content="RetroList | Retro Funding 5 Project Discovery and Community Voting UI" />
+    <meta name="description" content="RetroList | Retro Funding ${round} Project Discovery and Community Voting UI" />
   
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="https://retrolist.app/og/${projectId}.png" />
+    <meta property="fc:frame:image" content="${ogImg}" />
     <meta name="fc:frame:button:1" content="View Project" />
     <meta name="fc:frame:button:1:action" content="link" />
-    <meta name="fc:frame:button:1:target" content="https://retrolist.app/project/${projectId}" />
+    <meta name="fc:frame:button:1:target" content="https://round${round}.retrolist.app/project/${projectId}" />
     <meta name="fc:frame:button:2" content="Follow RetroList" />
     <meta name="fc:frame:button:2:action" content="link" />
     <meta name="fc:frame:button:2:target" content="https://warpcast.com/retrolist" />
