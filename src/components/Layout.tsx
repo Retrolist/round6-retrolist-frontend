b8033fb7346541384aeb2377ce16d699afc94c56
“Chinapanda” <kittikornchina2014@gmail.com>
2024-10-10 22:49:53 +0700

    feat: Implement rounds in navbar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,22 +4,54 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 import OptimismLogo from "./OptimismLogo";
 
+const link = [
+  "https://round3.retrolist.app/",
+  "https://round4.retrolist.app/",
+  "https://round5.retrolist.app/",
+  "https://round6.retrolist.app/",
+  // "https://round7.retrolist.app/",
+];
+
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const [isOpen, setIsOpen] = React.useState(false);
+
   return (
     <div className="">
       {/* Navbar */}
       <div className="border-b border-[#EAECF0]">
-        <div className="flex justify-between items-center p-4 container 2xl:max-w-[1440px] mx-auto relative">
+        <div className="flex justify-between items-center p-4 container 2xl:max-w-[1440px] mx-auto relative z-50">
           <div className="hidden sm:flex gap-5 text-base font-semibold absolute left-1/2 -translate-x-1/2">
             <Link to="/" className="py-2 px-3">
               <div>Projects</div>
             </Link>
-            <Link to="https://round4.retrolist.app" className="py-2 px-3">
-              <div>Round 4</div>
-            </Link>
-            <Link to="https://round3.retrolist.app" className="py-2 px-3">
-              <div>Round 3</div>
-            </Link>
+            <div
+              className="py-2 px-3 flex gap-2 items-center relative cursor-pointer z-50"
+              onMouseEnter={() => setIsOpen(true)}
+              onMouseLeave={() => setIsOpen(false)}
+            >
+              <div>Rounds</div>
+              <img src="/chevron-down.svg" alt="" />
+              {isOpen && (
+                <div
+                  onMouseEnter={() => setIsOpen(true)}
+                  onMouseLeave={() => setIsOpen(false)}
+                  className="absolute left-0 w-48 mt-2 top-10 bg-white border border-gray-300 rounded-md shadow-lg cursor-pointer z-50"
+                >
+                  <div className="py-1">
+                    {link.map((ele, index) => {
+                      return (
+                        <a
+                          href={ele}
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 cursor-pointer"
+                        >
+                          Rounds {index + 3}
+                        </a>
+                      );
+                    })}
+                  </div>
+                </div>
+              )}
+            </div>
             {/* <Link to="/list" className="py-2 px-3">
                 <div>Lists</div>
               </Link> */}
